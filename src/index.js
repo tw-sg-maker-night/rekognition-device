4402a04cf43c8879d3968ac724310d60b5059102
Niko <niko.wah.hn@gmail.com>
2017-02-03 17:45:22 +0800

    change sourceinput to local image

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 require('dotenv').config();
 const Express = require('express');
 const AWS = require('aws-sdk');
+const fs = require('fs');
 const app = Express();
 
 const port = process.env.PORT || 3000;
 const s3Bucket = process.env.AWS_BUCKET_NAME;
-const localDirectory = process.env.LOCAL_DIRECTORY
-const similarityThreshold = process.env.SIMILARITY_THRESHOLD
+const localDirectory = process.env.LOCAL_DIRECTORY;
+const similarityThreshold = process.env.SIMILARITY_THRESHOLD;
 
 AWS.config.update({region: process.env.AWS_REGION});
 const rekognition = new AWS.Rekognition();
 const s3 = new AWS.S3({Bucket:s3Bucket});
 
 app.get('/', (req, res) => {
-  var params = {
+  let sourceImage = fs.readFileSync(localDirectory + 'angelina-jolie2.jpeg', 'base64',
+    function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      return data;
+    });
+
+  let params = {
     SimilarityThreshold: similarityThreshold,
     SourceImage: {
-      S3Object: {
-        Bucket: s3Bucket,
-        Name: 'angelina-jolie.jpeg'
-      }
+      Bytes: new Buffer(sourceImage, 'base64')
     },
     TargetImage: {
      S3Object: {
@@ -30,7 +36,6 @@ app.get('/', (req, res) => {
    };
 
    rekognition.compareFaces(params, function(err, data) {
-     
      if (err) console.log(err, err.stack); // an error occurred
      else     console.log(data);           // successful response
    });
