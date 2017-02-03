92b1c69156c57159232540e3a45ee4d1d2f7e33d
Niko <niko.wah.hn@gmail.com>
2017-02-03 16:10:42 +0800

    test comparison with s3 bucket as both source and target

---
+++ README.md
@@ -0,0 +1,12 @@
+# AWS Rekognition with Webcam?
+
+Maybe using Raspberry pi with webcam and motion sensor to capture faces and compare it with our S3 bucket `rekognition-people`
+
+## Some environment variables you may need
+AWS_BUCKET_NAME=rekognition-people
+AWS_REGION=us-east-1
+SIMILARITY_THRESHOLD=80
+
+## Get Started
+1. npm start
+2. go to `localhost:3000`
--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
-const express = require('express');
+require('dotenv').config();
+const Express = require('express');
+const AWS = require('aws-sdk');
+const app = Express();
 
-const app = express();
 const port = process.env.PORT || 3000;
+const s3Bucket = process.env.AWS_BUCKET_NAME;
+const localDirectory = process.env.LOCAL_DIRECTORY
+const similarityThreshold = process.env.SIMILARITY_THRESHOLD
+
+AWS.config.update({region: process.env.AWS_REGION});
+const rekognition = new AWS.Rekognition();
+const s3 = new AWS.S3({Bucket:s3Bucket});
 
 app.get('/', (req, res) => {
-  console.log('hello');
-})
+  var params = {
+    SimilarityThreshold: similarityThreshold,
+    SourceImage: {
+      S3Object: {
+        Bucket: s3Bucket,
+        Name: 'angelina-jolie.jpeg'
+      }
+    },
+    TargetImage: {
+     S3Object: {
+      Bucket: s3Bucket,
+      Name: 'angelina-jolie.jpeg'
+     }
+    }
+   };
+
+   rekognition.compareFaces(params, function(err, data) {
+     
+     if (err) console.log(err, err.stack); // an error occurred
+     else     console.log(data);           // successful response
+   });
+});
 
 app.listen(port);
