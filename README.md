b60b0be24453770e36b912e0d3c272554bf6311a
Niko <niko.wah.hn@gmail.com>
2017-02-04 15:39:10 +0800

    change target image from one particular image to all images in a collection

--- README.md
+++ README.md
@@ -3,10 +3,44 @@
 Maybe using Raspberry pi with webcam and motion sensor to capture faces and compare it with our S3 bucket `rekognition-people`
 
 ## Some environment variables you may need
-AWS_BUCKET_NAME=rekognition-people
-AWS_REGION=us-east-1
-SIMILARITY_THRESHOLD=80
+```
+  //fixed settings
+  AWS_REGION=us-east-1
+  AWS_COLLECTION_ID=rekognition-people
+  SIMILARITY_THRESHOLD=75
+
+  LOCAL_DIRECTORY=/path/to/your/file/dir
+```
 
 ## Get Started
 1. npm start
 2. go to `localhost:3000`
+
+
+### Side Notes
+CollectionArn: aws:rekognition:us-east-1:623790533772:collection/rekognition-people
+AWS BUCKET: rekognition-people
+
+
+### AWS CLI COMMANDS
+Doesn't seem like there's a way to do it via the AWS Management (or maybe I can't find it)
+
+0. Upload your image to S3 bucket: `rekognition-people`
+1. List collections
+  * `aws rekognition list-collections`
+2. (If no collection found) Create a collection
+  * `aws rekognition create-collection --collection-id "rekognition-people"`
+3. List faces
+  * `aws rekognition list-faces --collection-id rekognition-people`
+4. Add Faces to collection
+  * ```
+    aws rekognition index-faces \
+    --image '{"S3Object":{"Bucket":"rekognition-people","Name":"Donnie-Yen.jpeg"}}' \
+    --collection-id "rekognition-people"
+    ```
+5. Delete Faces from collection
+  * ```
+    aws rekognition delete-faces \
+    --face-ids "id1,id2" \
+    --collection-id "rekognition-people"
+    ```
--- src/index.js
+++ src/index.js
@@ -5,37 +5,30 @@ const fs = require('fs');
 const app = Express();
 
 const port = process.env.PORT || 3000;
-const s3Bucket = process.env.AWS_BUCKET_NAME;
-const localDirectory = process.env.LOCAL_DIRECTORY;
+const collectionId = process.env.AWS_COLLECTION_ID;
 const similarityThreshold = process.env.SIMILARITY_THRESHOLD;
+const localDirectory = process.env.LOCAL_DIRECTORY;
 
 AWS.config.update({region: process.env.AWS_REGION});
 const rekognition = new AWS.Rekognition();
-const s3 = new AWS.S3({Bucket:s3Bucket});
 
 app.get('/', (req, res) => {
   let sourceImage = fs.readFileSync(localDirectory + 'angelina-jolie2.jpeg', 'base64',
     function (err,data) {
-      if (err) {
-        return console.log(err);
-      }
-      return data;
+      if (err) console.log(err);
+      else return data;
     });
 
   let params = {
-    SimilarityThreshold: similarityThreshold,
-    SourceImage: {
+    CollectionId: collectionId,
+    Image: {
       Bytes: new Buffer(sourceImage, 'base64')
     },
-    TargetImage: {
-     S3Object: {
-      Bucket: s3Bucket,
-      Name: 'angelina-jolie.jpeg'
-     }
-    }
+    FaceMatchThreshold: similarityThreshold,
+    MaxFaces: 1
    };
 
-   rekognition.compareFaces(params, function(err, data) {
+   rekognition.searchFacesByImage(params, function(err, data) {
      if (err) console.log(err, err.stack); // an error occurred
      else     console.log(data);           // successful response
    });
