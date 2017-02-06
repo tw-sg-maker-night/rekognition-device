e51c5044d0cf891886a6eb4f2b7c5e7b4e640761
Niko <niko.wah.hn@gmail.com>
2017-02-06 15:58:00 +0800

    Get file header data from file in s3 based on face matched

--- src/index.js
+++ src/index.js
@@ -8,17 +8,19 @@ const port = process.env.PORT || 3000;
 const collectionId = process.env.AWS_COLLECTION_ID;
 const similarityThreshold = process.env.SIMILARITY_THRESHOLD;
 const localDirectory = process.env.LOCAL_DIRECTORY;
+const bucketName = process.env.AWS_BUCKET;
 
 AWS.config.update({region: process.env.AWS_REGION});
 const rekognition = new AWS.Rekognition();
+const s3 = new AWS.S3();
 
 app.get('/', (req, res) => {
-  let sourceImage = fs.readFileSync(localDirectory + 'angelina-jolie2.jpeg', 'base64',
-    function (err,data) {
-      if (err) console.log(err);
-      else return data;
-    });
+  findAMatchInAwsFaces(getSourceImage());
+});
 
+app.listen(port);
+
+function findAMatchInAwsFaces(sourceImage) {
   let params = {
     CollectionId: collectionId,
     Image: {
@@ -26,12 +28,46 @@ app.get('/', (req, res) => {
     },
     FaceMatchThreshold: similarityThreshold,
     MaxFaces: 1
-   };
+  };
 
-   rekognition.searchFacesByImage(params, function(err, data) {
-     if (err) console.log(err, err.stack);
-     else     console.log(data.FaceMatches[0].Face);          
-   });
-});
+  rekognition.searchFacesByImage(params, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else {
+      if(data.FaceMatches.length>0)
+        processMatch(data.FaceMatches[0].Face);
+      else
+        console.log('No Match Found');
+    }
+  });
+}
 
-app.listen(port);
+function getSourceImage(){
+  return fs.readFileSync(
+    localDirectory + 'angelina-jolie2.jpeg', 'base64',
+    function (err,data) {
+      if (err) console.log(err);
+      else return data;
+    }
+  );
+}
+
+function processMatch(matched){
+  let params = {
+    Bucket: bucketName,
+    Key: matched.ExternalImageId
+  };
+  s3.headObject(params, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else {
+      actions(data.Metadata);
+    }
+  });
+}
+
+function actions(data) {
+  playBirthdaySong(data.birthday);
+}
+
+function playBirthdaySong(birthday){
+  console.log(birthday);
+}
